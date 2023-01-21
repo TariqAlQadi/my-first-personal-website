5525b1eaa5f5737c68be9f45e4c3b9c9f1cae594
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-21 10:49:11 +0100

    add theme picker

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Navigation
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
 
@@ -10,3 +11,31 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+//Color Picker
+
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+//store theme
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+//apply theme
+const retrieveTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+  });
+});
+
+//so it retrieves theme on first load of the page
+document.onload = retrieveTheme();
